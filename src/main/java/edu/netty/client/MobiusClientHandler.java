65074b190cc7c439950fec004eb9ec2a5df8f5d8
kostyantyn-garanskiy-mobius <kostyantyn.garanskiy@mobius-software.com>
2024-09-25 13:54:33 +0300

    added client structure

---
+++ src/main/java/edu/netty/client/MobiusClientHandler.java
@@ -0,0 +1,33 @@
+package edu.netty.client;
+
+import edu.netty.common.message.Message;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class MobiusClientHandler extends ChannelInboundHandlerAdapter{
+	
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+	    System.out.println("channelActive");
+	    super.channelActive(ctx);
+	    ctx.writeAndFlush("message from client");
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+	    System.out.println("[]");
+	    super.channelRead(ctx, msg);
+
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+	    System.out.println("exceptionCaught");
+	    super.exceptionCaught(ctx, cause);
+
+	    cause.printStackTrace();
+	    ctx.close();
+
+	}
+
+}
